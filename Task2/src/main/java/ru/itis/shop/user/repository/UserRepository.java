9eae85f6f36edbadd8f7869dd8b4ce4c3831453e
NinjaMasterStas <egorovklastanislav123321@gmail.com>
2026-06-30 12:51:58 +0300

    Was added updating profile and was edited getEmailById

---
+++ Task2/src/main/java/ru/itis/shop/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ru.itis.shop.user.repository;
+
+import ru.itis.shop.user.domain.User;
+
+import java.util.Optional;
+
+public interface UserRepository {
+    void save(User user);
+    void updateProfileDescription(String email, String newProfileDescription);
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(String id);
+}
